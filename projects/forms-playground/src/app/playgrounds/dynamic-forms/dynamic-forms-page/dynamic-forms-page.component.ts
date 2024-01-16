d6724fd4964bf3823e02f838127487c2fcad017d
Oron Segev <segev.oron@gmail.com>
2024-01-16 23:30:56 +0200

    add basic buttons and layout

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable, Subject, switchMap, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dynamic-forms-page',
@@ -8,4 +10,15 @@ import { CommonModule } from '@angular/common';
   templateUrl: './dynamic-forms-page.component.html',
   styleUrls: ['../../common-page.scss', './dynamic-forms-page.component.scss'],
 })
-export class DynamicFormsPageComponent {}
+export class DynamicFormsPageComponent implements OnInit {
+  protected formLoadingTrigger = new Subject<'user' | 'company'>();
+  protected fromConfig$!: Observable<object>;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fromConfig$ = this.formLoadingTrigger.pipe(
+      switchMap(config => this.http.get(`assets/${config}.form.json`))
+    );
+  }
+}
