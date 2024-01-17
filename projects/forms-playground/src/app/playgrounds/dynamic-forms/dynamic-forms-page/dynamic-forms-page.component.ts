c3553f3bcffed6ef6ec397ad2a6538606003733a
Oron Segev <segev.oron@gmail.com>
2024-01-18 00:10:53 +0200

    render the form

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-forms-page/dynamic-forms-page.component.ts
@@ -2,23 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, Subject, switchMap, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { DynamicFormConfig } from './models/dynamic-forms.model';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-forms-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './dynamic-forms-page.component.html',
   styleUrls: ['../../common-page.scss', './dynamic-forms-page.component.scss'],
 })
 export class DynamicFormsPageComponent implements OnInit {
+  form!: FormGroup;
+
   protected formLoadingTrigger = new Subject<'user' | 'company'>();
-  protected fromConfig$!: Observable<object>;
+  protected fromConfig$!: Observable<DynamicFormConfig>;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.fromConfig$ = this.formLoadingTrigger.pipe(
-      switchMap(config => this.http.get(`assets/${config}.form.json`))
+      switchMap((config) =>
+        this.http.get<DynamicFormConfig>(`assets/${config}.form.json`)
+      ),
+      tap(({ controls }) => {
+        this.buildForm(controls);
+      })
     );
+  }
+
+  protected onSumbit() {
+    console.log(this.form.value);
+    this.form.reset();
+  }
+
+  private buildForm(controls: DynamicFormConfig['controls']) {
+    this.form = new FormGroup({});
+    Object.keys(controls).forEach((key) => {
+      this.form.addControl(key, new FormControl(controls[key].value));
+    });
+    console.log(this.form);
   }
 }
