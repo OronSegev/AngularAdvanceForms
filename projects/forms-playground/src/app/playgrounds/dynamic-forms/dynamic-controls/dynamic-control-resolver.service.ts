4996c6722f3640613f7fcad19d384c12b4afdf81
Oron Segev <segev.oron@gmail.com>
2024-02-05 19:34:58 +0200

    lazy load form components

--- projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-controls/dynamic-control-resolver.service.ts
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-controls/dynamic-control-resolver.service.ts
@@ -4,23 +4,28 @@ import { DynamicInputComponent } from './dynamic-input.component';
 import { DynamicSelectComponent } from './dynamic-select.component';
 import { DynamicCheckboxComponent } from './dynamic-checkbox.component';
 import { DynamicGroupComponent } from './dynamic-group.component';
+import { from, of, tap } from 'rxjs';
 
 type DynamicControlMap = {
-  [T in DynamicControl['controlType']]: Type<any>;
+  [T in DynamicControl['controlType']]: () => Promise<Type<any>>;
 };
 
 @Injectable({
   providedIn: 'root'
 })
 export class DynamicControlResolver {
-  private controlComponents: DynamicControlMap = {
-    input: DynamicInputComponent,
-    select: DynamicSelectComponent,
-    checkbox: DynamicCheckboxComponent,
-    group: DynamicGroupComponent
+  private lazyControlComponents: DynamicControlMap = {
+    input: () => import('./dynamic-input.component').then(c => c.DynamicInputComponent),
+    select: () => import('./dynamic-select.component').then(c => c.DynamicSelectComponent),
+    checkbox: () => import('./dynamic-checkbox.component').then(c => c.DynamicCheckboxComponent),
+    group: () => import('./dynamic-group.component').then(c => c.DynamicGroupComponent)
   }
+  private loadedControlComponents = new Map<string, Type<any>>();
 
   resolve(controlType: keyof DynamicControlMap) {
-    return this.controlComponents[controlType];
+    const loadedComponent = this.loadedControlComponents.get(controlType);
+    if (loadedComponent) return of(loadedComponent);
+
+    return from(this.lazyControlComponents[controlType]()).pipe(tap(comp => this.loadedControlComponents.set(controlType, comp)));
   }
 }
