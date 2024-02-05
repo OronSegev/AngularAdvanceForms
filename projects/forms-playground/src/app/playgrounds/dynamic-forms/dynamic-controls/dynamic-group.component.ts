8732d82221d15cde4eae47ee90ed2f3a2a816aaa
Oron Segev <segev.oron@gmail.com>
2024-02-06 00:44:13 +0200

    add dynamic group control

---
+++ projects/forms-playground/src/app/playgrounds/dynamic-forms/dynamic-controls/dynamic-group.component.ts
@@ -0,0 +1,28 @@
+import { Component, HostBinding, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseDynamicControl, dynamicControlProvider } from './base-dynamic-control';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ControlInjectorPipe } from "../control-injector.pipe";
+import { DynamicControlResolver } from './dynamic-control-resolver.service';
+
+@Component({
+    selector: 'app-dynamic-group',
+    standalone: true,
+    imports: [CommonModule, ReactiveFormsModule, ControlInjectorPipe],
+    viewProviders: [dynamicControlProvider],
+    template: `
+      <fieldset [formGroupName]="control.controlKey">
+        <legend>{{ control.config.label }}</legend>
+        <ng-container class="form-field" *ngFor="let control of control.config.controls | keyvalue">
+          <ng-container
+          [ngComponentOutlet]="controlResolver.resolve(control.value.controlType) | async"
+          [ngComponentOutletInjector]="control.key | controlInjector:control.value"></ng-container>
+        </ng-container>
+      </fieldset>
+    `,
+    styles: []
+  })
+export class DynamicGroupComponent extends BaseDynamicControl {
+  @HostBinding('class') override hostClass = 'form-field-group'
+  controlResolver = inject(DynamicControlResolver);
+}
